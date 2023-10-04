b61a6d13a8ba7e62b419b1dc6b7438bedd955e72
remon zander <remon.zander1@gmail.com>
2023-10-04 15:22:34 +0200

    added dbservice

--- oefen project/server/src/app.ts
+++ oefen project/server/src/app.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import { AddressInfo } from "net";
+import { GetAll } from './services/DBservice';
 
 const app = express();
 
@@ -8,3 +9,5 @@ app.set('port', 3000);
 const server = app.listen(app.get('port'), function () {
     console.log(`Express server listening on port ${(server.address() as AddressInfo).port}`);
 });
+
+GetAll();
--- oefen project/server/src/services/DBservice.ts
+++ oefen project/server/src/services/DBservice.ts
@@ -1,3 +1,23 @@
-import mysql from 'mysql'
+const mysql = require('mysql');
 
-const connection: mysql = mysql
+const connection = mysql.createConnection({
+    host: '10.10.1.6',
+    user: 'Niek',
+    password: 'niek',
+    database: 'Niek'
+});
+
+connection.connect();
+
+
+export function GetAll() {
+    console.log("test");
+    connection.query(`SELECT * FROM Deelnemers`, function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+    });
+}
+
+module.exports = {
+    GetAll
+}
