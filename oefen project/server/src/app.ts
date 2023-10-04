53ab1d9a0cefab08ee673d63d705e09b6d04760c
niek221 <116267865+niek221@users.noreply.github.com>
2023-10-04 14:48:40 +0200

    Update

--- oefen project/server/src/app.ts
+++ oefen project/server/src/app.ts
@@ -1,12 +1,10 @@
 import * as express from 'express';
 import { AddressInfo } from "net";
 
-import debug = require('debug');
-
 const app = express();
 
 app.set('port', 3000);
 
 const server = app.listen(app.get('port'), function () {
-    debug(`Express server listening on port ${(server.address() as AddressInfo).port}`);
+    console.log(`Express server listening on port ${(server.address() as AddressInfo).port}`);
 });
